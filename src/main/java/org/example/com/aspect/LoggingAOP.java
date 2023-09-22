e43fbba201fcd6f5bcdab612378827cfd310b857
Natalie <natawerch@gmail.com>
2023-09-22 11:15:47 +0200

    commit

--- src/main/java/org/example/com/aspect/LoggingAOP.java
+++ src/main/java/org/example/com/aspect/LoggingAOP.java
@@ -1,5 +1,6 @@
 package org.example.com.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
  */
 @Aspect
 @Component
+@Slf4j
 public class LoggingAOP {
 
     /**
@@ -24,20 +26,20 @@ public class LoggingAOP {
     @Around("execution(* org.example.com.controller..*(..))")
     public Object logMethodExecution(ProceedingJoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
-        System.out.println("Before executing method: " + methodName);
+        log.info("Before executing method: " + methodName);
 
         long startTime = System.currentTimeMillis();
         Object result = null;
         try {
             result = joinPoint.proceed();
         } catch (Throwable e) {
-            System.out.println("Exception is method " + methodName + ". Exception: " + e.getMessage());
+            log.warn("Exception is method " + methodName + ". Exception: " + e.getMessage());
         } finally {
             long endTime = System.currentTimeMillis();
-            System.out.println("After executing method " + methodName);
-            System.out.println("Method " + methodName + " took " + (endTime - startTime) + " ms");
+            log.info("After executing method " + methodName);
+            log.info("Method " + methodName + " took " + (endTime - startTime) + " ms");
         }
-        System.out.println("After returning from method " + methodName + ". Result: " + result);
+        log.info("After returning from method " + methodName + ". Result: " + result);
         return result;
     }
 }
